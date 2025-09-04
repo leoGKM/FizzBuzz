50b59720f492ef41406fa702cf40f0492539116b
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-09-04 19:17:14 -0400

    se muestra la lista del numero ingresado

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,12 @@
-import sumar from "./sumador";
+function generateFizzBuzz() {
+    const number = parseInt(document.getElementById('number').value); 
+    const resultDiv = document.getElementById('result');
+    
+    let result = [];
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
+    for (let i = 1; i <= number; i++) {
+      result.push(i);
+    }
+    
+    resultDiv.innerHTML = result.join(', ');
+}
