b528b03dd28f78f0c07c0204286619176d2bc6ad
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-09-04 19:55:14 -0400

    se añadio la verificacion de un numero invalido

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,11 @@ function generateFizzBuzz() {
     const number = parseInt(document.getElementById('number').value); 
     const resultDiv = document.getElementById('result');
     
+    if (isNaN(number) || number <= 0) {
+        alert("Por favor ingresa un número válido.");
+        return;
+    }
+    
     let result = [];
 
     for (let i = 1; i <= number; i++) {
@@ -10,3 +15,4 @@ function generateFizzBuzz() {
     
     resultDiv.innerHTML = result.join(', ');
 }
+export {generateFizzBuzz};
