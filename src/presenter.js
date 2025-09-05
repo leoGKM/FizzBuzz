cfb973fe63046f32d2733a9a8d12f1bb2def33eb
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-09-04 20:01:25 -0400

    se añadio la palabra fizzbuzz al haber un divizor de 5 y 3

--- src/presenter.js
+++ src/presenter.js
@@ -10,14 +10,15 @@ function generateFizzBuzz() {
     let result = [];
 
     for (let i = 1; i <= number; i++) {
-      if (i % 3 === 0) {
+      if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
             result.push("Fizz");
-        }else if (i % 5 === 0) {
+        } else if (i % 5 === 0) {
             result.push("Buzz");
-        }else {
-          result.push(i);
+        } else {
+            result.push(i);
         }
-      
     }
     
     resultDiv.innerHTML = result.join(', ');
